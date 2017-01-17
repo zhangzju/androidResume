1652baff165745a32991cebb692be141e2ca00c6
zhangzju <zhangway19921221@163.com>
2017-01-18 00:16:44 +0800

    Generate app

--- app/src/main/java/com/zhangzju/helloworld/MainActivity.java
+++ app/src/main/java/com/zhangzju/helloworld/MainActivity.java
@@ -3,16 +3,19 @@ package com.zhangzju.helloworld;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.zhangzju.MESSAGE";
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "Zhangzju resume");
     }
 
     public void sendMessage(View view)
